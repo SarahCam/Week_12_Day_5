0ecb990910d2271682581e31305e952ca5cceea5
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-14 14:44:27 +0100

    Displaying some demographic data for selected country

--- api_homework/public/index.js
+++ api_homework/public/index.js
@@ -1,3 +1,45 @@
+// Key - Source : demographicsArray data.
+
+// birth_rate	Worldbank
+// capital_coordinates Wikipedia
+// capital_name	Wikipedia
+// death_rate	Worldbank
+// diabetes_prevalence Worldbank
+// economic_sectors	Fischer Weltalmanach
+// education_expenditure Worldbank
+// electric_energy_consumption Worldbank
+// forest_area	Returns the total amount of forest area in a country (in km²)	Worldbank
+// forest_area_percent	Returns the percentage of the land area covered by a forest for a country.	Worldbank
+// gdp_total	Returns the total Gross Domestic Product (GDP) for a country (unit: USD).	Worldbank
+// gdp_capita	Returns the Gross Domestic Product per person for a country (unit: USD).	Worldbank
+// gini	Returns the Gini coefficient. Worldbank
+// happiness_index	UNSDSN
+// health_expenditure	Worldbank
+// internetuser	Worldbank
+// internetusers_percent Worldbank
+// jobless_rate	Worldbank
+// life_expectancy UN-Data
+// literacy_rate Worldbank
+// medianwage	Worldbank
+// median_age	UN-Data
+// migration UN-Data
+// migration_rate	UN-Data
+// military_expenditure	Worldbank
+// mobile_cellular_subscriptions Worldbank
+// murder_rate UNODC
+// population	UN-Data
+// population_0_14 Worldbank
+// population_15_64	Worldbank
+// population_over_64	Worldbank
+// size	UN-Data
+// urban_population	Worldbank
+
+
+let demographicsArray = [];
+let countriesArray = [];
+
+let demographicsData = "population,murder_rate,migration,median_age,education_expenditure,density,population_over_64";
+
 // Create XHR to 'get' JSON data from API (url)
 // & fire a callback function once the data is loaded:
 const makeRequest = function(url, callback){
@@ -34,21 +76,33 @@ const createCountriesDropDown = function(array){
     option.value = arrayItem;
     option.innerText = arrayItem.name;            // restcountries data
     select.appendChild(option);
- // countryArray.push(arrayItem);                 // Add country to our countryArray
+    countriesArray.push(arrayItem);
   }
 };
 
 const createDemographicsDropDown = function(array){
-  console.log("HIYA");
   const select = document.getElementById('demographics-dropdown');
   for(let arrayItem of array){
-    console.log("HIYA 2", arrayItem);
     const option = document.createElement('option');
     option.value = arrayItem;
     option.innerText = arrayItem.countryName;     // inqstats data
     select.appendChild(option);
- // demoArray.push(arrayItem);                    // Add country to our countryArray
+    demographicsArray.push(arrayItem);
   }
+};
+
+// Create country method:
+const createCountry = function(demographic){
+  const countryName = document.getElementById('country-name');
+  countryName.innerText = demographic.countryName;
+  // const region = document.getElementById('region');
+  // region.innerText = country.countryName;
+  const population = document.getElementById('population');
+  population.innerText = "Population: " + demographic.population;
+  const murderRate = document.getElementById('murder-rate');
+  murderRate.innerText = "Murder Rate: " + demographic.murder_rate;
+  const medianAge = document.getElementById('median-age');
+  medianAge.innerText = "Median Age: " + demographic.median_age;
 };
 
 // Main function:
@@ -57,9 +111,15 @@ const app = function(){
   const urlCountries = "https://restcountries.eu/rest/v2/all";
   makeRequest(urlCountries, createCountries);
 
-  // http://blog.inqubu.com/inqstats: open-api-published-to-get-demographic-data
-  const urlDemographics = "http://inqstatsapi.inqubu.com/?api_key=8f4fe2162ce4d44d&cmd=getWorldData&data=population,murder_rate,migration,median_age,education_expenditure,density,population_over_64";
+  const urlDemographics = "http://inqstatsapi.inqubu.com/?api_key=8f4fe2162ce4d44d&cmd=getWorldData&data=" + demographicsData;
   makeRequest(urlDemographics, createDemographics);
+
+  const select = document.getElementById('demographics-dropdown');
+  select.addEventListener("change", function(event){
+    console.log(event);
+    console.log(event.target.selectedIndex);
+    createCountry(demographicsArray[event.target.selectedIndex]);
+  });
 
 }
 
